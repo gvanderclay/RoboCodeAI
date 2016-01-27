dda9b68e3cdd96cf8f7a38b0fee0194829ed00f7
couturmi <couturmi@mail.gvsu.edu>
2016-01-27 12:56:51 -0500

    added color changer

--- RamBeater.java
+++ RamBeater.java
@@ -1,21 +1,25 @@
 package RoboCodeAI;
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
+import java.util.Random;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 /**
- * RamBeater - a robot by Gage Vander Clay & Mitch Couturier
- * POOOOOOOOP
+ * RamBeater 
+* @author: Gage Vander Clay & Mitch Couturier
  */
 public class RamBeater extends Robot
 {
+	Random rn;
+	int color;
 	/**
 	 * run: RamBeater's default behavior
 	 */
 	public void run() {
 		// Initialization of the robot should be put here
-
+		rn = new Random();
+		color = 0;
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
@@ -23,11 +27,45 @@ public class RamBeater extends Robot
 
 		// Robot main loop
 		while(true) {
+			//Change Color
+			randomColors();
 			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			//ahead(100);
+			turnGunRight(10);
+			//back(100);
+			//turnGunRight(360);
+		}
+	}
+	
+	/**
+	 * Set random colors for the robot
+	 */
+	public void randomColors(){
+		if(color < 9)
+			color++;
+		else
+			color = 0;		
+
+		if(color == 0){
+			setAllColors(Color.white);
+		}else if(color == 1){
+			setAllColors(Color.black);
+		}else if(color == 2){
+			setAllColors(Color.red);
+		}else if(color == 3){
+			setAllColors(Color.orange);
+		}else if(color == 4){
+			setAllColors(Color.yellow);
+		}else if(color == 5){
+			setAllColors(Color.green);
+		}else if(color == 6){
+			setAllColors(Color.blue);
+		}else if(color == 7){
+			setAllColors(Color.cyan);
+		}else if(color == 8){
+			setAllColors(Color.pink);
+		}else if(color == 9){
+			setAllColors(Color.magenta);
 		}
 	}
 
